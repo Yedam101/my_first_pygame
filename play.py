4564a044e964893168427ad0a361d444b1d654da
Yedam101 <waterclean101@gmail.com>
2023-03-02 10:04:10 +0900

    0302

--- play.py
+++ play.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 def display_score():
     curr_time = int(pygame.time.get_ticks() / 100) - start_time
@@ -9,6 +10,16 @@ def display_score():
     # pygame.draw.rect(screen, 'green', score_rect, 10)
     screen.blit(score_surf, score_rect)
     return curr_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 2
+            screen.blit(enamy_surf, obstacle_rect)
+        return obstacle_list
+    
+    else: return []
+
     
 
 pygame.init() 
@@ -29,7 +40,9 @@ ground_surface = pygame.image.load('graphics/ground2.png').convert_alpha()
 # max_surf = test_font.render(f'BEST: {max_score}', False, 'Blue')
 
 enamy_surf = pygame.image.load("graphics/snail/walk01.png").convert_alpha()
-enamy_rect = enamy_surf.get_rect(center = (500, 300))
+enamy_rect = enamy_surf.get_rect(center = (randint(500, 900), 300))
+# obstacle
+obstacle_rect_list = []
 
 player_surf = pygame.image.load("graphics/Player/Skeleton_Walk1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80,330))
@@ -43,7 +56,13 @@ player_stand_rect = player_stand.get_rect(center = (380,210))
 game_name = test_font.render('Pixel Jumper', False, 'black')
 game_name_rect = game_name.get_rect(center = (420,120))
 
+# timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 900)
+
+
 while True:
+    # event loop
     for event in pygame.event.get(): # 창 닫기 버튼
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -64,6 +83,9 @@ while True:
                 enamy_rect.left = 800
                 player_rect.left = 0
                 start_time = int(pygame.time.get_ticks() / 100)
+
+        if event.type == obstacle_timer:
+                obstacle_rect_list.append(enamy_surf.get_rect(center = (randint(500,1000), 300)))
                          
                 
     if game_active:
@@ -79,10 +101,11 @@ while True:
         max_surf = test_font.render(f'BEST: {max_score}', False, 'gold') 
         screen.blit(max_surf, (30, 20)) # best score 표시
 
-        enamy_rect.x -= 3
-        if enamy_rect.x < -50:
-            enamy_rect.x = 800
-        screen.blit(enamy_surf, enamy_rect)
+        # timer 때문에 더 이상 필요 x
+        # enamy_rect.x -= 3
+        # if enamy_rect.x < -50:
+        #     enamy_rect.x = 800
+        # screen.blit(enamy_surf, enamy_rect)
 
         # Player
         player_rect.x += 4
@@ -96,6 +119,8 @@ while True:
             player_rect.bottom = 330 # 바닥에 붙이기
         screen.blit(player_surf, player_rect)
 
+        # obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
         
         if enamy_rect.colliderect(player_rect):
             store_score.append(display_score())
